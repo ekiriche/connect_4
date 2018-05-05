f48232a16ffbeb15d2783e4fece04b813f2c8981
Evgeniy KIRICHENKO <ekiriche@e1r2p7.unit.ua>
2018-05-05 13:11:44 +0300

    asd

---
+++ main.c
@@ -0,0 +1,177 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekiriche <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/05 11:48:39 by ekiriche          #+#    #+#             */
+/*   Updated: 2018/05/05 12:04:05 by ekiriche         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./ft_printf/ft_printf.h"
+
+int		is_map_won(char **map)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (map[i] != NULL)
+	{
+		j = 0;
+		while (map[i][j] != '\0')
+		{
+			if (map[i] && map[i + 1] && map[i + 2] && map[i + 3] &&
+				map[i][j] == 'X' && map[i + 1][j] == 'X' && map[i + 2][j] == 'X' &&
+				map[i + 3][j] == 'X')
+				return (1);
+			if (map[i] && map[i + 1] && map[i + 2] && map[i + 3] &&
+				map[i][j] == 'O' && map[i + 1][j] == 'O' && map[i + 2][j] == 'O' &&
+				map[i + 3][j] == 'O')
+				return (2);
+			if (map[i][j] && map[i][j + 1] && map[i][j + 2] && map[i][j + 3] &&
+				map[i][j] == 'X' && map[i][j + 1] == 'X' && map[i][j + 2] == 'X' &&
+				map[i][j + 3] == 'X')
+				return (1);
+			if (map[i][j] && map[i][j + 1] && map[i][j + 2] && map[i][j + 3] &&
+				map[i][j] == 'O' && map[i][j + 1] == 'O' && map[i][j + 2] == 'O' &&
+				map[i][j + 3] == 'O')
+				return (2);
+			if (map[i] && map[i + 1] && map[i + 2] && map[i + 3] &&
+				map[i][j] && map[i + 1][j + 1] && map[i + 2][j + 2] && map[i + 3][j + 3] &&
+				map[i][j] == 'X' && map[i + 1][j + 1] == 'X' && map[i + 2][j + 2] == 'X' &&
+				map[i + 3][j + 3] == 'X')
+				return (1);
+			if (map[i] && map[i + 1] && map[i + 2] && map[i + 3] &&
+				map[i][j] && map[i + 1][j + 1] && map[i + 2][j + 2] && map[i + 3][j + 3] &&
+				map[i][j] == 'O' && map[i + 1][j + 1] == 'O' && map[i + 2][j + 2] == 'O' &&
+				map[i + 3][j + 3] == 'O')
+				return (2);
+			if (map[i] && map[i + 1] && map[i + 2] && map[i + 3] &&
+				map[i][j] && map[i + 1][j - 1] && map[i + 2][j - 2] && map[i + 3][j - 3] &&
+				map[i][j] == 'X' && map[i + 1][j - 1] == 'X' && map[i + 2][j - 2] == 'X' &&
+				map[i + 3][j - 3] == 'X')
+				return (1);
+			if (map[i] && map[i + 1] && map[i + 2] && map[i + 3] &&
+				map[i][j] && map[i + 1][j - 1] && map[i + 2][j - 2] && map[i + 3][j - 3] &&
+				map[i][j] == 'O' && map[i + 1][j - 1] == 'O' && map[i + 2][j - 2] == 'O' &&
+				map[i + 3][j - 3] == 'O')
+				return (2);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int		height_of_array(char **map)
+{
+	int count;
+
+	count = 0;
+	while (map[count] != NULL)
+		count++;
+	return (count);
+}
+
+int		can_play(char **map, int j)
+{
+	if (map[0][j] != '.')
+		return (-1);
+	return (1);
+}
+
+void	play(char **map, int j, char c)
+{
+	int i;
+
+	i = height_of_array(map) - 1;
+	while (i >= 0)
+	{
+		if (map[i][j] == '.')
+		{
+			map[i][j] = c;
+			return ; 
+		}
+		i--;
+	}
+}
+
+int	find_errors(char *str1, char *str2)
+{
+	char *temp1;
+	char *temp2;
+
+	temp1 = ft_itoa(ft_atoi(str1));
+	temp2 = ft_itoa(ft_atoi(str2));
+	if (ft_strcmp(temp1, str1) != 0 || ft_strcmp(temp2, str2) != 0 ||
+		ft_atoi(str1) < 6 || ft_atoi(str2) < 7)
+		return (1);
+	return (0);
+}
+
+char **create_map(int x, int y)
+{
+	char **ans;
+	int i;
+	int j;
+
+	ans = (char**)malloc(sizeof(char*) * (y + 1));
+	i = 0;
+	while (i < y)
+	{
+		j = 0;
+		ans[i] = (char*)malloc(sizeof(char) * (x + 1));
+		while (j < x)
+		{
+			ans[i][j] = '.';
+			j++;
+		}
+		ans[i][j] = '\0';
+		i++;
+	}
+	ans[i] = NULL;
+	return (ans);
+}
+
+int main(int argc, char **argv)
+{
+	char **map;
+
+	if (argc != 3 || find_errors(argv[1], argv[2]))
+	{
+		write(2, "Error\n", 6);
+		return (0);
+	}
+	map = create_map(ft_atoi(argv[1]), ft_atoi(argv[2]));
+	int i = 0;
+	char buff[10];
+	int turn = 0;
+	while (1)
+	{
+		i = 0;
+		read(0, buff, 10);
+		if (turn % 2 == 0)
+			play(map, ft_atoi(buff), 'X');
+		else
+			play(map, ft_atoi(buff), 'O');
+		turn++;
+		if (is_map_won(map) == 1)
+		{
+			ft_printf("Player won!\n");
+			return (0);
+		}
+		else if (is_map_won(map) == 2)
+		{
+			ft_printf("AI won!\n");
+			return (0);
+		}
+		while (map[i])
+		{
+			ft_putendl(map[i]);
+			i++;
+		}
+	}
+}
